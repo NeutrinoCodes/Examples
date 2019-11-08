d0cc45b8ad660fc2a97a5733be503407ce709fa9
Erik Zorzin <erik@zorzin.com>
2019-11-08 11:39:43 +0100

    Trying using Neutrino as a library.

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -39,8 +39,8 @@
 #define KERNEL_F2     "thekernel2.cl"                                                               // OpenCL kernel.
 
 // INCLUDES:
-#include "opengl.hpp"                                                                               // Neutrino's OpenGL context declarations.
-#include "opencl.hpp"                                                                               // Neutrino's OpenCL context declarations.
+#include "nu.hpp"                                                                                   // Neutrino's OpenGL context declarations.
+// #include "opencl.hpp"                                                                               // Neutrino's OpenCL context declarations.
 
 int main ()
 {
