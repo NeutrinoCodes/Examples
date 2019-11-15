d646f2b1b4d37a1ca7c53207b315b28a9cd79dd7
ezor <erik@zorzin.com>
2019-11-15 14:18:16 +0100

    SINUSOID: rearranged directories. It works. Tested on Linux.

---
+++ Cloth/Code/cmake/CMakeFiles/cloth.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cloth.dir/src/main.cpp.o"
+  "cloth"
+  "cloth.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cloth.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
