809602efdc9eb63e74b0d23e37b8152bd09c0c5c
Erik Zorzin <erik@zorzin.com>
2020-05-22 22:59:44 +0200

    Working on multiple builds.

--- Cloth/Code/src/main.cpp
+++ Cloth/Code/src/main.cpp
@@ -7,18 +7,18 @@
 #define GUI_NAME      "Neutrino - Cloth"                                                            // Window name.
 
 #ifdef __linux__
-  #define SHADER_HOME "../../shader"                                                                // Linux OpenGL shaders directory.
-  #define KERNEL_HOME "../../kernel"                                                                // Linux OpenCL kernels directory.
+  #define SHADER_HOME "../Cloth/Code/shader"                                                        // Linux OpenGL shaders directory.
+  #define KERNEL_HOME "../Cloth/Code/kernel"                                                        // Linux OpenCL kernels directory.
 #endif
 
 #ifdef __APPLE__
-  #define SHADER_HOME "../../shader"                                                                // Mac OpenGL shaders directory.
-  #define KERNEL_HOME "../../kernel"                                                                // Mac OpenCL kernels directory.
+  #define SHADER_HOME "../Cloth/Code/shader"                                                        // Mac OpenGL shaders directory.
+  #define KERNEL_HOME "../Cloth/Code/kernel"                                                        // Mac OpenCL kernels directory.
 #endif
 
 #ifdef WIN32
-  #define SHADER_HOME "..\\..\\shader"                                                              // Windows OpenGL shaders directory.
-  #define KERNEL_HOME "..\\..\\kernel"                                                              // Windows OpenCL kernels directory.
+  #define SHADER_HOME "..\\Cloth\\Code\\shader"                                                     // Windows OpenGL shaders directory.
+  #define KERNEL_HOME "..\\Cloth\\Code\\kernel"                                                     // Windows OpenCL kernels directory.
 #endif
 
 #define SHADER_VERT   "voxel_vertex.vert"                                                           // OpenGL vertex shader.
