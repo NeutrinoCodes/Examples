453b1bc585bcca2badc5b20915ef01364e4c7994
Erik Zorzin <erik@zorzin.com>
2020-09-04 23:17:38 +0200

    Working on geo files in mesh example.

--- Mesh/Code/src/main.cpp
+++ Mesh/Code/src/main.cpp
@@ -27,7 +27,7 @@
 #define SHADER_VERT   "voxel_vertex.vert"                                                           // OpenGL vertex shader.
 #define SHADER_GEOM   "voxel_geometry.geom"                                                         // OpenGL geometry shader.
 #define SHADER_FRAG   "voxel_fragment.frag"                                                         // OpenGL fragment shader.
-#define GMSH_MESH     "Chain.msh"                                                                   // GMSH mesh.
+#define GMSH_MESH     "Cube.msh"                                                                    // GMSH mesh.
 
 // OPENCL:
 #define QUEUE_NUM     1                                                                             // # of OpenCL queues [#].
