48ed61c6b0f7b22f0511c306d325a9647416b5ca
ezor <erik@zorzin.com>
2021-03-28 15:55:39 +0200

    Updated sinusoid to new neutrino namespace.

--- Sinusoid/Code/src/main.cpp
+++ Sinusoid/Code/src/main.cpp
@@ -7,20 +7,15 @@
 #define SX            800                                                                           // Window x-size [px].
 #define SY            600                                                                           // Window y-size [px].
 #define NAME          "Neutrino - Sinusoid"                                                         // Window name.
-#define ORB_X         0.0f                                                                          // x-axis orbit initial rotation.
-#define ORB_Y         0.0f                                                                          // y-axis orbit initial rotation.
-#define PAN_X         0.0f                                                                          // x-axis pan initial translation.
-#define PAN_Y         0.0f                                                                          // y-axis pan initial translation.
-#define PAN_Z         -2.0f                                                                         // z-axis pan initial translation.
+#define ORBX          0.0f                                                                          // x-axis orbit initial rotation.
+#define ORBY          0.0f                                                                          // y-axis orbit initial rotation.
+#define PANX          0.0f                                                                          // x-axis pan initial translation.
+#define PANY          0.0f                                                                          // y-axis pan initial translation.
+#define PANZ          -2.0f                                                                         // z-axis pan initial translation.
 
 #ifdef __linux__
   #define SHADER_HOME "../Sinusoid/Code/shader/"                                                    // Linux OpenGL shaders directory.
   #define KERNEL_HOME "../Sinusoid/Code/kernel/"                                                    // Linux OpenCL kernels directory.
-#endif
-
-#ifdef __APPLE__
-  #define SHADER_HOME "../Sinusoid/Code/shader/"                                                    // Mac OpenGL shaders directory.
-  #define KERNEL_HOME "../Sinusoid/Code/kernel/"                                                    // Mac OpenCL kernels directory.
 #endif
 
 #ifdef WIN32
@@ -38,47 +33,42 @@
 
 int main ()
 {
-  // DATA:
-  float      x_min          = -1.0f;                                                                // "x_min" spatial boundary [m].
-  float      x_max          = +1.0f;                                                                // "x_max" spatial boundary [m].
-  float      y_min          = -1.0f;                                                                // "y_min" spatial boundary [m].
-  float      y_max          = +1.0f;                                                                // "y_max" spatial boundary [m].
-  size_t     nodes_x        = 100;                                                                  // Number of nodes in "X" direction [#].
-  size_t     nodes_y        = 100;                                                                  // Number of nodes in "Y" direction [#].
-  size_t     nodes          = nodes_x*nodes_y;                                                      // Total number of nodes [#].
-  float      dx             = (x_max - x_min)/(nodes_x - 1);                                        // x-axis mesh spatial size [m].
-  float      dy             = (y_max - y_min)/(nodes_y - 1);                                        // y-axis mesh spatial size [m].
-  size_t     i              = 0;                                                                    // "x" direction index.
-  size_t     j              = 0;                                                                    // "y" direction index.
-  nu_float4* color          = new nu_float4 (0);                                                    // Color [].
-  nu_float4* position       = new nu_float4 (1);                                                    // Position [m].
-  nu_float*  t              = new nu_float (2);                                                     // Time step [s].
-
-  // GUI PARAMETERS (orbit):
-  float      orbit_x_init   = 0.0f;                                                                 // x-axis orbit initial rotation.
-  float      orbit_y_init   = 0.0f;                                                                 // y-axis orbit initial rotation.
-
-  // GUI PARAMETERS (pan):
-  float      pan_x_init     = 0.0f;                                                                 // x-axis pan initial translation.
-  float      pan_y_init     = 0.0f;                                                                 // y-axis pan initial translation.
-  float      pan_z_init     = -2.0f;                                                                // z-axis pan initial translation.
+  // INDICES:
+  size_t      i              = 0;                                                                   // "x" direction index.
+  size_t      j              = 0;                                                                   // "y" direction index.
 
   // MOUSE PARAMETERS:
-  float      ms_orbit_rate  = 1.0f;                                                                 // Orbit rotation rate [rev/s].
-  float      ms_pan_rate    = 5.0f;                                                                 // Pan translation rate [m/s].
-  float      ms_decaytime   = 1.25f;                                                                // Pan LP filter decay time [s].
+  float       ms_orbit_rate  = 1.0f;                                                                // Orbit rotation rate [rev/s].
+  float       ms_pan_rate    = 5.0f;                                                                // Pan translation rate [m/s].
+  float       ms_decaytime   = 1.25f;                                                               // Pan LP filter decay time [s].
 
   // GAMEPAD PARAMETERS:
-  float      gmp_orbit_rate = 1.0f;                                                                 // Orbit angular rate coefficient [rev/s].
-  float      gmp_pan_rate   = 1.0f;                                                                 // Pan translation rate [m/s].
-  float      gmp_decaytime  = 1.25f;                                                                // Low pass filter decay time [s].
-  float      gmp_deadzone   = 0.1f;                                                                 // Gamepad joystick deadzone [0...1].
+  float       gmp_orbit_rate = 1.0f;                                                                // Orbit angular rate coefficient [rev/s].
+  float       gmp_pan_rate   = 1.0f;                                                                // Pan translation rate [m/s].
+  float       gmp_decaytime  = 1.25f;                                                               // Low pass filter decay time [s].
+  float       gmp_deadzone   = 0.30f;                                                               // Gamepad joystick deadzone [0...1].
 
-  // NEUTRINO:
-  opengl*    gl             = new opengl (NAME, SX, SY, ORB_X, ORB_Y, PAN_X, PAN_Y, PAN_Z);         // OpenGL context.
-  opencl*    cl             = new opencl (NU_GPU);                                                  // OpenCL context.
-  shader*    S              = new shader ();                                                        // OpenGL shader program.
-  kernel*    K              = new kernel ();                                                        // OpenCL kernel array.
+  // OPENGL:
+  nu::opengl* gl             = new nu::opengl (NAME, SX, SY, ORBX, ORBY, PANX, PANY, PANZ);         // OpenGL context.
+  nu::shader* S              = new nu::shader ();                                                   // OpenGL shader program.
+
+  // OPENCL:
+  nu::opencl* cl             = new nu::opencl (NU_GPU);                                             // OpenCL context.
+  nu::kernel* K              = new nu::kernel ();                                                   // OpenCL kernel array.
+  nu::float4* color          = new nu::float4 (0);                                                  // Color [].
+  nu::float4* position       = new nu::float4 (1);                                                  // Position [m].
+  nu::float1* t              = new nu::float1 (2);                                                  // Time step [s].
+
+  // SIMULATION:
+  float       x_min          = -1.0f;                                                               // "x_min" spatial boundary [m].
+  float       x_max          = +1.0f;                                                               // "x_max" spatial boundary [m].
+  float       y_min          = -1.0f;                                                               // "y_min" spatial boundary [m].
+  float       y_max          = +1.0f;                                                               // "y_max" spatial boundary [m].
+  size_t      nodes_x        = 100;                                                                 // Number of nodes in "X" direction [#].
+  size_t      nodes_y        = 100;                                                                 // Number of nodes in "Y" direction [#].
+  size_t      nodes          = nodes_x*nodes_y;                                                     // Total number of nodes [#].
+  float       dx             = (x_max - x_min)/(nodes_x - 1);                                       // x-axis mesh spatial size [m].
+  float       dy             = (y_max - y_min)/(nodes_y - 1);                                       // y-axis mesh spatial size [m].
 
   ////////////////////////////////////////////////////////////////////////////////////////////////////
   ///////////////////////////////////////// DATA INITIALIZATION //////////////////////////////////////
@@ -164,6 +154,7 @@ int main ()
   delete K;                                                                                         // Deleting OpenCL kernel...
   delete position;                                                                                  // Deleting OpenGL point...
   delete color;                                                                                     // Deleting OpenGL color...
+  delete t;                                                                                         // Deleting time...
 
   return 0;
 }
