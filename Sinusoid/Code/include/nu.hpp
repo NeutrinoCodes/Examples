bf684a7ceb019c012335e5426bd758ffdb9cf9e9
ezor <erik@zorzin.com>
2021-05-06 15:21:28 +0200

    Working on Windows.

---
+++ Sinusoid/Code/include/nu.hpp
@@ -0,0 +1,17 @@
+/// @file     nu.hpp
+/// @author   Erik ZORZIN
+/// @date     14NOV2019
+/// @brief    Header file for Neutrino library.
+///
+/// @details  This is the header file that must be included in all user's
+/// Neutrino based applications.
+
+#ifndef nu_hpp
+#define nu_hpp
+
+// INCLUDES:
+  #include "mesh.hpp"                                                                               // Neutrino's mesh context declarations.
+  #include "opengl.hpp"                                                                             // Neutrino's OpenGL context declarations.
+  #include "opencl.hpp"                                                                             // Neutrino's OpenCL context declarations.
+
+#endif
